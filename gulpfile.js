f02aaea6de424e1ac8aaac9e0e8d6aeb3ff79e2b
nicoconti1901 <nico.conti.1901@gmail.com>
2024-05-06 19:11:59 -0300

    finalizada la seccion boletos

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-import { src, dest, watch} from "gulp";
+import { src, dest, watch } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 
 const sass = gulpSass(dartSass);
 
-export function css( done ) {
-  src('src/scss/app.scss')
-      .pipe( sass().on('error', sass.logError) )
-      .pipe( dest('build/css') )
+export function css(done) {
+  src("src/scss/app.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("build/css", { sourcemaps: true }));
 
-  done()
+  done();
 }
 
 export function dev() {
-  watch('src/scss/**/*.scss', css)
+  watch("src/scss/**/*.scss", css);
 }
