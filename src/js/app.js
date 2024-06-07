c1bedc9aabf8cb70f8d658566b9de2e2bf29f1f4
nicoconti1901 <nico.conti.1901@gmail.com>
2024-06-07 11:16:07 -0300

    funciones del scroll terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
+  navegacionFija();
   crearGaleria();
+  resaltarEnlace();
+  scrollNav();
 });
 
+function navegacionFija() {
+  // Navegacion fija al hacer scroll
+  const header = document.querySelector(".header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+
+  document.addEventListener("scroll", function () {
+    if (sobreFestival.getBoundingClientRect().bottom < 1) {
+      header.classList.add("fixed");
+    } else {
+      header.classList.remove("fixed");
+    }
+  });
+}
+
+function scrollNav() {
+  const navLinks = document.querySelectorAll(".navegacion-principal a");
+
+  navLinks.forEach( link => {
+    link.addEventListener("click", e => {
+      e.preventDefault();
+
+      const sectionScroll = e.target.getAttribute("href"); 
+      const seccion = document.querySelector(sectionScroll);
+      seccion.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+} // Scroll suave
+
+function resaltarEnlace() {
+  // Resalta enlace actual segun el scroll
+  document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".navegacion-principal a");
+    let actual = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        actual = section.id;
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + actual) {
+        link.classList.add("active");
+      }
+    });
+  });
+}
+
 function crearGaleria() {
+  // Crea la galeria de imagenes
   const cantidadImagenes = 16;
   const galeria = document.querySelector(".galeria-imagenes");
 
@@ -21,8 +80,9 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-  const imagen = document.createElement("IMG");
-  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  // Muestra la imagen en un modal
+  const imagen = document.createElement("IMG"); //Crear imagen
+  imagen.src = `src/img/gallery/full/${i}.jpg`; //Asignar imagen
 
   //Crear el modal
   const modal = document.createElement("DIV");
@@ -46,6 +106,7 @@ function mostrarImagen(i) {
 }
 
 function cerrarModal() {
+  // Cierra el modal
   const modal = document.querySelector(".modal");
   modal.classList.add("fade-out");
 
